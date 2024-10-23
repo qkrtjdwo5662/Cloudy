fbd63d18fcdc7991e94dd293b75f95825b55f739
LimGayoung <ygy920@gmail.com>
2024-10-23 16:55:28 +0900

    init: backend 프로젝트 생성

---
+++ backend/cloudy/src/main/java/com/cloudy/CloudyApplication.java
@@ -0,0 +1,13 @@
+package com.cloudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudyApplication.class, args);
+	}
+
+}
---
+++ backend/cloudy/src/test/java/com/cloudy/CloudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
