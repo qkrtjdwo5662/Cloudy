c1a9a320dac7984fa18216e58e2d3177ea36a167
dayoungpyo <dayoungpyo@gmail.com>
2024-10-25 17:05:38 +0900

    feat: Main page sementic markup example

--- frontend/cloudy/src/app/page.tsx
+++ frontend/cloudy/src/app/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
     <>
-      <section className="flex w-full h-full gap-10">
-        <article className="flex w-2/3 h-full bg-white border border-gray-200 rounded-5"></article>
-        <aside className="flex w-1/3 h-full bg-white border border-gray-200 rounded-5"></aside>
+      <section className="flex h-full w-full gap-10">
+        <article className="flex h-full w-2/3 rounded-5 border border-gray-200 bg-white"></article>
+        <aside className="flex h-full w-1/3 rounded-5 border border-gray-200 bg-white"></aside>
       </section>
-      <section className="flex w-full h-full gap-10">
-        <article className="flex w-1/4 h-full bg-white border border-gray-200 rounded-5"></article>
-        <article className="flex w-1/4 h-full bg-white border border-gray-200 rounded-5"></article>
-        <article className="flex w-1/4 h-full bg-white border border-gray-200 rounded-5"></article>
-        <article className="flex w-1/4 h-full bg-white border border-gray-200 rounded-5"></article>
+      <section className="flex h-full w-full gap-10">
+        <article className="flex h-full w-1/4 rounded-5 border border-gray-200 bg-white"></article>
+        <article className="flex h-full w-1/4 rounded-5 border border-gray-200 bg-white"></article>
+        <article className="flex h-full w-1/4 rounded-5 border border-gray-200 bg-white"></article>
+        <article className="flex h-full w-1/4 rounded-5 border border-gray-200 bg-white"></article>
       </section>
     </>
   );
