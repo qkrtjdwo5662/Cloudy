62145297a31aa13819468f4f8518d0424dd9a0d3
dayoungpyo <dayoungpyo@gmail.com>
2024-10-25 16:59:24 +0900

    feat: Main page sementic markup example

--- frontend/cloudy/src/app/page.tsx
+++ frontend/cloudy/src/app/page.tsx
@@ -1,5 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <>
+      <section className="flex w-full h-full gap-10">
+        <article className="flex w-2/3 h-full bg-white border border-gray-200 rounded-5"></article>
+        <aside className="flex w-1/3 h-full bg-white border border-gray-200 rounded-5"></aside>
+      </section>
+      <section className="flex w-full h-full gap-10">
+        <article className="flex w-1/4 h-full bg-white border border-gray-200 rounded-5"></article>
+        <article className="flex w-1/4 h-full bg-white border border-gray-200 rounded-5"></article>
+        <article className="flex w-1/4 h-full bg-white border border-gray-200 rounded-5"></article>
+        <article className="flex w-1/4 h-full bg-white border border-gray-200 rounded-5"></article>
+      </section>
+    </>
+  );
 }
