da41702044f978b5e8a832555bf405fcf970b1ac
dayoungpyo <dayoungpyo@gmail.com>
2024-10-29 13:02:33 +0900

    feat: Title 컴포넌트 마크업

---
+++ frontend/cloudy/src/shared/ui/Title/Title.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+import Button from "../Button/Button";
+
+type Size = "l" | "m";
+
+interface TitleProps {
+  size: Size;
+  showButton?: boolean;
+  buttonContent?: string;
+  buttonIcon?: string;
+  children: ReactNode;
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+const Title = ({
+  size,
+  showButton,
+  buttonContent,
+  buttonIcon,
+  children,
+  onClick,
+}: TitleProps) => {
+  const styles = {
+    size: {
+      l: "text-indigo-500 text-4xl font-bold",
+      m: "text-indigo-500 text-2xl",
+    },
+  };
+
+  const classes = [styles.size[size]].join(" ");
+  return (
+    <div className="flex items-center justify-between pl-4 pr-2 pt-9">
+      <span className={classes}>{children}</span>
+      {showButton && (
+        <Button
+          size={size === "l" ? "m" : "s"}
+          variant="primary"
+          design="fill"
+          type="button"
+          mainText={buttonContent}
+          rightIcon={buttonIcon}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Title;
