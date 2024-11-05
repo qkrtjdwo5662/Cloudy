d1946911a32b6202a58875b3e589af854c70c468
dayoungpyo <dayoungpyo@gmail.com>
2024-11-05 15:20:27 +0900

    fix: layout 수정

--- frontend/cloudy/app/layout.tsx
+++ frontend/cloudy/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "../src/shared/globals.css";
-
+import { NavigationBox } from "@/shared/ui";
 export const metadata: Metadata = {
   title: "Cloudy",
   description: "Cloudy",
@@ -13,13 +13,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
-          rel="stylesheet"
-        />
-      </head>
-      <body>{children}</body>
+      <body>
+        <div className="flex h-full gap-20 bg-stone-100 pr-20">
+          <aside className="flex h-full w-300 flex-col border-r border-gray-200 bg-white">
+            <nav></nav>
+          </aside>
+
+          <div className="flex h-full w-full flex-col gap-10">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
