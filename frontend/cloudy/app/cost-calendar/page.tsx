42ee82b22753a3ac598ad51c7ba1147094a84e9d
dayoungpyo <dayoungpyo@gmail.com>
2024-11-06 14:30:46 +0900

    feat: Calendar 추가

--- frontend/cloudy/app/cost-calendar/page.tsx
+++ frontend/cloudy/app/cost-calendar/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Table from "../server-usage/table";
 import CostBarChart from "./barChart";
+import Calendar from "../dashboard/Calendar";
 
 const sampleData = [
   { containerName: "컨테이너1", callCount: "14회" },
@@ -12,25 +13,25 @@ const sampleData = [
 
 export default function CostCalendarPage() {
   return (
-    <div className="flex h-full w-full bg-gray-100">
+    <div className="flex h-620 w-full bg-gray-100">
       <div className="flex h-full w-full flex-col gap-6 p-20">
         <header className="flex items-center justify-between pb-6">
           <h1 className="text-3xl font-bold text-indigo-500">비용 캘린더</h1>
         </header>
 
         <div className="flex h-full flex-col gap-6">
-          <section className="flex h-full w-full flex-row gap-6">
-            <div className="flex w-full rounded-lg border border-gray-200 bg-white p-4"></div>
-            <div className="flex w-full rounded-lg border border-gray-200 bg-white p-4">
+          <section className="flex h-1/2 w-full flex-row gap-6">
+            <div className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white p-20">
+              <Calendar />
+            </div>
+            <div className="flex w-full rounded-lg border border-gray-200 bg-white p-20">
               <Table rows={sampleData} />
             </div>
           </section>
 
-          <aside className="flex h-full gap-6">
-            <div className="w-full rounded-lg border border-gray-200 bg-white p-4">
-              <CostBarChart />
-            </div>
-          </aside>
+          <div className="h-full w-full rounded-lg border border-gray-200 bg-white p-20">
+            <CostBarChart />
+          </div>
         </div>
       </div>
     </div>
