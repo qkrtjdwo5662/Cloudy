830a0e6680971c3c87a1205d72d798d135c93ecb
LimGayoung <ygy920@gmail.com>
2024-11-06 14:02:01 +0900

    fix: company 삭제

--- backend/cloudy/src/main/java/com/cloudy/domain/company/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cloudy.domain.company.repository;
-
-import com.cloudy.domain.company.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-}
