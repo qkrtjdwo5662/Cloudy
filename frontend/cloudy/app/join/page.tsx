61e12aef9a7bd272b1f2ee52811b17661b19557d
김현지 <chik1y@naver.com>
2024-11-08 13:07:59 +0900

    fix: 가입 페이지 수정

--- frontend/cloudy/app/join/page.tsx
+++ frontend/cloudy/app/join/page.tsx
@@ -5,20 +5,14 @@ import { Title } from "@/shared/ui/title/Title";
 export default function JoinPage() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="flex w-1/2 flex-col items-center justify-center rounded-10 border border-gray-200 bg-white py-15">
+      <div className="flex w-1/2 flex-col items-center justify-center rounded-10 border border-gray-200 bg-white py-30 shadow-lg">
         <Title size="l">가입하기</Title>
 
         <form className="flex w-3/5 flex-col gap-12 p-8">
-          <Input
-            label="기업명"
-            placeholder="placeholder"
-            warning="warning"
-            showStar={true}
-          />
+          <Input label="기업명" placeholder="placeholder" showStar={true} />
           <Input
             label="사업자 등록 번호"
             placeholder="placeholder"
-            warning="warning"
             showButton
             showStar={true}
             buttonContent="중복확인"
