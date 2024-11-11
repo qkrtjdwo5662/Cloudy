a3d0004cfab75afad92d8d9c4674cc2a6deba348
GoDongHyun <att815@naver.com>
2024-11-11 14:08:31 +0900

    fix: login

--- backend/cloudy/src/main/java/com/cloudy/domain/member/controller/AuthController.java
+++ backend/cloudy/src/main/java/com/cloudy/domain/member/controller/AuthController.java
@@ -34,7 +34,7 @@ public class AuthController {
     @Operation(summary = "로그인 API", description = "MemberLoginRequest로 로그인 진행")
     @SwaggerApiSuccess(description = "로그인 성공")
     @SwaggerApiError({ErrorCode.NOT_EXIST_MEMBER, ErrorCode.NOT_MATCH_PASSWORD})
-    @GetMapping("/login")
+    @PostMapping("/login")
     public Response<MemberLoginResponse> login(@Valid @RequestBody MemberLoginRequest memberLoginRequest) {
 //        System.out.println(memberLoginRequest.getLoginId());
         MemberLoginResponse memberLoginResponse = authService.login(memberLoginRequest);
