588561a2de2435b3190296011673fbd238387f18
GoDongHyun <att815@naver.com>
2024-11-12 14:10:30 +0900

    feat: alarm sse

--- backend/cloudy/src/main/java/com/cloudy/domain/alarm/service/AlarmService.java
+++ backend/cloudy/src/main/java/com/cloudy/domain/alarm/service/AlarmService.java
@@ -2,6 +2,7 @@ package com.cloudy.domain.alarm.service;
 
 import com.cloudy.domain.alarm.model.dto.request.AlarmCreateRequest;
 import com.cloudy.domain.alarm.model.dto.response.AlarmResponse;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface AlarmService {
     AlarmResponse createAlarm(AlarmCreateRequest request, Long memberId); // 알람 생성
     AlarmResponse getAlarmDetail(Long alarmId); // 알람 상세 조회
     void markAlarmAsRead(Long alarmId, Long memberId); // 알람 읽음 처리
+    SseEmitter subscribe(Long memberId);
+    void sendAlarm(Long memberId, AlarmResponse alarmResponse);
 }
