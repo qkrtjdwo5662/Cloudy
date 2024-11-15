dd0c8b9752f6386c844cbe0238f8f9926c9bf3ea
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2024-11-15 18:30:04 +0900

    feat: 프로젝트 추가

---
+++ backend/commercial/src/main/java/com/commercial/domain/auth/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.commercial.domain.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @PostMapping("/sms/send")
+    public ResponseEntity<?> sendSms() {
+        log.info("method: {}, API: {}, http_method: {}, external_service: {}, container: {}",
+                "sendSms", "/auth/sms/send", "POST", true, "commercial-be");
+        return ResponseEntity.ok("Post Request");
+    }
+
+    @PostMapping("/sms/verify")
+    public ResponseEntity<?> verifySms() {
+        log.info("method: {}, API: {}, http_method: {}, external_service: {}, container: {}",
+                "verifySms", "/auth/sms/verify", "POST", true, "commercial-be");
+        return ResponseEntity.ok("Post Request");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login() {
+        log.info("method: {}, API: {}, http_method: {}, external_service: {}, container: {}",
+                "login", "/auth/login", "POST", false, "commercial-be");
+        return ResponseEntity.ok("Post Request");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register() {
+        log.info("method: {}, API: {}, http_method: {}, external_service: {}, container: {}",
+                "register", "/auth/register", "POST", false, "commercial-be");
+        return ResponseEntity.ok("Post Request");
+    }
+
+}
